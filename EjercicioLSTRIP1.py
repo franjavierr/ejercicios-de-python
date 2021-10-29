26c5f22306c5547352a59da842b66f83c130a815
Francisco Javier Rodríguez Durán <franjavierrodriguezduran@gmail.com>
2021-10-29 09:27:16 +0200

    Create EjercicioLSTRIP.py

---
+++ EjercicioLSTRIP1.py
@@ -0,0 +1,3 @@
+cadenaejemplo = " En un lugar de la mancha" print(cadenaejemplo.lstrip()) 
+cadenaejemplo = "En un lugar de la mancha " print(cadenaejemplo.rstrip()) 
+cadenaejemplo = " En un lugar de la mancha " print(cadenaejemplo.strip())
